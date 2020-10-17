82fb4b29eb12de91bb7198a7b7f25a22cf67295f
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2020-10-17 15:23:00 +0900

    ログイン画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#create'
+  post '/login', to: 'sessions#destroy'
   root 'top#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
