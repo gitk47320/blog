c1e72ca8f6ce0567e61a72726e89bda5e468839f
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2020-10-18 18:13:19 +0900

    sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/login', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
   root 'top#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
