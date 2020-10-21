d525238e470cdf0d06d2d1639ab64ef3952ff8ce
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2020-10-21 23:40:00 +0900

    サインアップ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :show]
+  
   def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   private
@@ -8,5 +24,9 @@ class UsersController < ApplicationController
     unless logged_in?
       redirect_to login_path
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :login_id, :email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
+  root 'top#index'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  root 'top#index'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
