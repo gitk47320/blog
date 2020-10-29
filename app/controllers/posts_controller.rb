da6f5dc37fc02e909d626861d32b2b4ff34fda67
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2020-10-30 00:00:29 +0900

    記事表示少し

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
 
+  def show
+  end
+
   def new
     @post = Post.new
   end
