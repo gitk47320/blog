2765cff5c868a4af53fce2c98ed2a6492fd9212d
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2020-10-29 23:24:06 +0900

    記事投稿機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :show]
   
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(id: current_user.id)
   end
 
   def new
