edaded011aac4f0a902fbac550def5bf2972ccf8
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2020-11-29 17:31:17 +0900

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'test', login_id: 'testid', password: 'password')
+User.create(login_id: 'testid1', password: 'password1', email: 'testid1@example.com')
+User.find(1).create_profile(nickname: 'テスト１',selfintro: '25歳です。よろしく')
+User.find(1).posts.create(title: 'はじめまして', post: 'ブログはじめました')
+User.find(1).posts.create(title: '寒いですね', post: '最近寒いですね。長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト')
+
+User.create(login_id: 'testid2', password: 'password2', email: 'testid2@example.com')
+User.find(2).create_profile(nickname: 'テスト２',selfintro: 'よろしくお願いします。')
+User.find(2).posts.create(title: '自己紹介的なもの', post: 'ブログはじめました。私について知ってほしいです。')
+User.find(2).posts.create(title: '私の趣味について', post: '今日は私の趣味である読書について語ります。。長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト')
+
+User.create(login_id: 'testid3', password: 'password3', email: 'testid3@example.com')
+User.find(3).create_profile(nickname: 'テスト３',selfintro: 'とうとうブログをはじめました！よろしくですー')
+User.find(3).posts.create(title: 'こんにちわ！', post: 'ブログはじめました')
+User.find(3).posts.create(title: 'スポーツ観戦してきた！', post: 'サッカー見に行ってきたよ！。長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト長文テスト')
+
