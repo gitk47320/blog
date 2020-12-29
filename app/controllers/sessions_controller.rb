a748f083dc982433cf27f5666c35d8b39fd52733
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2020-12-29 12:15:34 +0900

    commit test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,16 @@ class SessionsController < ApplicationController
     @user = User.find_by(login_id: params[:session][:login_id])
     if @user && @user.authenticate(params[:session][:password])
       log_in(@user)
-      redirect_to @user
+      redirect_to @user, success: 'ログインしました'
     else
-      render new
+      flash.now[:danger] = 'ログインに失敗しました。'
+      render :new
     end
   end
 
   def destroy
     session.clear
     current_user = nil
-    redirect_to login_path
+    redirect_to login_path, success: 'ログアウトしました'
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def flash_message
+    flash.map do | type, message |
+      content_tag :div, message, class: [:alert, "alert-#{type}"]
+    end.join
+  end
 end
