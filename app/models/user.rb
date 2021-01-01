8cec05815ba2cacd30dc18e3b004ab35e02f57f3
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2021-01-01 13:56:21 +0900

    アカウント作成のバリデーションチェック

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts, dependent: :destroy
   has_one :profile, dependent: :destroy
+  validates :email, presence: true
+  validates :login_id, presence: true
+  validates :password, presence: true
 end
