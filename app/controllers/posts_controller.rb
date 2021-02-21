1cc885494d776cb32e734dd81995a232413a18ea
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2021-02-21 15:52:55 +0900

    余分なコードを削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,6 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @comments = @post.comments
-    logger.debug("aaaaaaaaaaaa#{@comments.present?}")
     @comment = @post.comments.build
   end
 
