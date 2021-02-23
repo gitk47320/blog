37e05582735d91d40568a3db958b603047faa987
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2021-02-23 16:58:09 +0900

    user_spec修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,19 +8,32 @@ RSpec.describe User, type: :model do
 
   it "is not valid user's email" do
     user = User.new(email: 'test.example.com', login_id: 'example', password: 'password1234')
-    user.valid?
-    expect(user.errors.messages[:email]).to include('は不正な値です')
+    expect(user.valid?).to eq false
   end
 
   it "is not valid user's login_id" do
     user = User.new(email: 'test@example.com', login_id: 'ああああああ', password: 'password1234')
     user.valid?
-    expect(user.errors.messages[:login_id]).to include('は不正な値です')
+    expect(user.valid?).to eq false
   end
 
   it "is not valid user's password" do
     user = User.new(email: 'test@example.com', login_id: 'example', password: 'パスワード')
     user.valid?
-    expect(user.errors.messages[:password]).to include('は不正な値です')
+    expect(user.valid?).to eq false
+  end
+
+  it "is unique user's email" do
+    User.create(email: 'test@example.com', login_id: 'example', password: 'password')
+    user = User.new(email: 'test@example.com', login_id: 'example1', password: 'password')
+    user.valid?
+    expect(user.valid?).to eq false
+  end
+
+  it "is unique user's login_id" do
+    User.create(email: 'test@example.com', login_id: 'example', password: 'password')
+    user = User.new(email: 'test1@example.com', login_id: 'example', password: 'password')
+    user.valid?
+    expect(user.valid?).to eq false
   end
 end
