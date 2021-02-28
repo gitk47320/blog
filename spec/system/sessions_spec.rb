c955d93c0d29f8dcdf8de146bc205aaba2dcd0a5
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2021-03-01 00:55:17 +0900

    ログイン失敗のシステムテスト

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -14,4 +14,19 @@ describe 'ログインする', type: :system, js: true do
       expect(page).to have_content 'ログインしました'
     end
   end
+
+  describe 'ログイン失敗' do
+    before do 
+      user = FactoryBot.create(:user)
+    end
+
+    it 'failed to login' do
+      visit login_path
+      fill_in 'ログインID', with: 'test02'
+      fill_in 'パスワード', with: 'password02'
+      click_button 'ログイン'
+      expect(page).to have_content 'IDまたはパスワードが不正のためログインできませんでした'
+    end
+  end
 end
+
